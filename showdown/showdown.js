1c691fcaa414b466d950913911293b85b2bc07ca
dsm122n <dsm122n@gmail.com>
2023-10-18 18:07:02 -0300

    fix css

--- showdown/showdown.js
+++ showdown/showdown.js
@@ -8,9 +8,36 @@ document.addEventListener('DOMContentLoaded', function() {
       });
       
       // Convert Markdown to HTML
-      const html = converter.makeHtml(markdownText);
+      var html = converter.makeHtml(markdownText);
     
       // Insert the HTML into the container
       document.getElementById('markdown-medi').innerHTML = html;
+      
     });
-});
+    const buttonPediatria = document.querySelector('#button-pediatria');
+    buttonPediatria.addEventListener('click', event => {
+      event.preventDefault();
+      var referencia = buttonPediatria.getAttribute('referencia');
+      console.log(referencia);
+      for (let i = 0; i < cambioAtributoElements.length; i++) {
+        cambioAtributoElements[i].setAttribute('src', referencia);
+    }
+      fetch(referencia)
+        .then(response => response.text())
+        .then(markdownText => {
+          // Create a new instance of Showdown converter
+          const converter = new showdown.Converter({
+          tables: true,
+          });
+          
+          // Convert Markdown to HTML
+          var html = converter.makeHtml(markdownText);
+          console.log(href)
+          console.log(html);
+        
+          // Insert the HTML into the container
+          document.getElementsByClassName('md-cuerpo').innerHTML = html;
+        });
+    });
+  });
+
