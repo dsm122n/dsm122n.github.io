20c4a62ec51345d09366a71336be49592796234e
dsm122n <dsm122n@gmail.com>
2023-10-18 20:34:40 -0300

    nav bar working

--- pediatria/pediatria.md
+++ pediatria/pediatria.md
@@ -1,2 +1,42 @@
-# aquí va todo pediatría :D
+# Pediatría
 
+## datitos antropometría
+
+### mujeres
+| edad | peso | aumento de peso | talla |  aumento de talla | Perímetro cefálico |
+|------|------|-------|-------------|--------------------| --------|
+| 0-1 mes | 3.2-4.2 kg | 1 kg/mes | 50-54 cm | 4-5 cm/mes | 34-37 cm |
+| 1-2 meses | 4.2-5.2 kg | 1 kg/mes | 54-58 cm | 3-4 cm/mes | 37-39 cm |
+| 2-3 meses | 5.2-5.8 kg | 0.5 kg/mes | 58-60 cm | 2-3 cm/mes | 39-41 cm |
+| 3-6 meses | 5.8-7.0 kg | 0.5 kg/mes | 60-66 cm | 2-3 cm/mes | 41-43 cm |
+| 6-12 meses | 7.0-9.0 kg | 0.25 kg/mes | 66-74 cm | 1-2 cm/mes | 43-45 cm |
+| 1-2 años | 9.0-12.0 kg | 0.25 kg/mes | 74-84 cm | 1-2 cm/mes | 45-47 cm |
+| 2-3 años | 12.0-14.0 kg | 0.25 kg/mes | 84-94 cm | 1-2 cm/mes | 47-49 cm |
+| 3-4 años | 14.0-16.0 kg | 0.25 kg/mes | 94-104 cm | 1-2 cm/mes | 49-51 cm |
+| 4-5 años | 16.0-18.0 kg | 0.25 kg/mes | 104-114 cm | 1-2 cm/mes | 51-53 cm |
+
+
+### hombres
+| edad | peso | aumento de peso | talla |  aumento de talla | Perímetro cefálico |
+|------|------|-------|-------------|--------------------| --------|
+| 0-1 mes | 3.4-4.4 kg | 1 kg/mes | 51-55 cm | 4-5 cm/mes | 34-37 cm |
+| 1-2 meses | 4.4-5.4 kg | 1 kg/mes | 55-59 cm | 3-4 cm/mes | 37-39 cm |
+| 2-3 meses | 5.4-6.0 kg | 0.5 kg/mes | 59-61 cm | 2-3 cm/mes | 39-41 cm |
+| 3-6 meses | 6.0-7.2 kg | 0.5 kg/mes | 61-67 cm | 2-3 cm/mes | 41-43 cm |
+| 6-12 meses | 7.2-9.5 kg | 0.25 kg/mes | 67-75 cm | 1-2 cm/mes | 43-45 cm |
+| 1-2 años | 9.5-12.5 kg | 0.25 kg/mes | 75-85 cm | 1-2 cm/mes | 45-47 cm |
+| 2-3 años | 12.5-14.5 kg | 0.25 kg/mes | 85-95 cm | 1-2 cm/mes | 47-49 cm |
+| 3-4 años | 14.5-16.5 kg | 0.25 kg/mes | 95-105 cm | 1-2 cm/mes | 49-51 cm |
+| 4-5 años | 16.5-18.5 kg | 0.25 kg/mes | 105-115 cm | 1-2 cm/mes | 51-53 cm |
+
+
+Perímetro cefálico para la talla:
+
+$ PCT = frac{Talla}{2} + 10 \pm 1 $
+
+# Infectología
+## Enfermedad de kawasaki
+![kawasaki](image.png)
+
+
+Infiltrado perihiliar + Banda presidencial en ADV RxTx
--- showdown/showdown.js
+++ showdown/showdown.js
@@ -1,3 +1,19 @@
+// Get the content of the <div> element
+const mathContent = document.getElementById("math-content").innerHTML;
+
+// Define a regular expression to match LaTeX equations within $$
+const latexRegex = /\$\$(.*?)\$\$/g;
+
+// Use replace() with a callback function to render each equation
+const renderedContent = mathContent.replace(latexRegex, (match, latex) => {
+  const container = document.createElement("span");
+  katex.render(latex, container);
+  return container.outerHTML;
+});
+
+// Set the modified content back to the <div>
+document.getElementById("math-content").innerHTML = renderedContent;
+
 document.addEventListener('DOMContentLoaded', function() {
     fetch('medi.md')
     .then(response => response.text())
@@ -11,33 +27,28 @@ document.addEventListener('DOMContentLoaded', function() {
       var html = converter.makeHtml(markdownText);
     
       // Insert the HTML into the container
-      document.getElementById('markdown-medi').innerHTML = html;
+      document.getElementById('markdown-main').innerHTML = html;
       
     });
-    const buttonPediatria = document.querySelector('#button-pediatria');
-    buttonPediatria.addEventListener('click', event => {
-      event.preventDefault();
-      var referencia = buttonPediatria.getAttribute('referencia');
-      console.log(referencia);
-      for (let i = 0; i < cambioAtributoElements.length; i++) {
-        cambioAtributoElements[i].setAttribute('src', referencia);
-    }
-      fetch(referencia)
-        .then(response => response.text())
-        .then(markdownText => {
-          // Create a new instance of Showdown converter
-          const converter = new showdown.Converter({
-          tables: true,
-          });
-          
-          // Convert Markdown to HTML
-          var html = converter.makeHtml(markdownText);
-          console.log(href)
-          console.log(html);
-        
-          // Insert the HTML into the container
-          document.getElementsByClassName('md-cuerpo').innerHTML = html;
-        });
-    });
+   
   });
 
+function loadMD(filepath) {
+  fetch(filepath)
+    .then(response => response.text())
+    .then(markdownText => {
+      // Create a new instance of Showdown converter
+      const converter = new showdown.Converter({
+      tables: true,
+      });
+      
+      // Convert Markdown to HTML
+      var html = converter.makeHtml(markdownText);
+    
+      // Insert the HTML into the container
+      document.getElementById('markdown-main').innerHTML = html;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
