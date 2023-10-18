18ce1fdeebcbb827a9fc7e1d7b0ad2636b7cc16b
dsm122n <dsm122n@gmail.com>
2023-10-17 22:37:58 -0300

    added pediatria

---
+++ pediatria/pediatria.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('medi.md')
+    .then(response => response.text())
+    .then(markdownText => {
+      // Create a new instance of Showdown converter
+      const converter = new showdown.Converter({
+      tables: true,
+      });
+      
+      // Convert Markdown to HTML
+      const html = converter.makeHtml(markdownText);
+    
+      // Insert the HTML into the container
+      document.getElementById('markdown-medi').innerHTML = html;
+    });
+});
