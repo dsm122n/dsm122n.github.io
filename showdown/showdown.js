df386ed4544ff6c0a5c4f1539c01d01997e27156
dsm122n <dsm122n@gmail.com>
2023-10-24 22:10:01 -0300

    dosis primero

--- showdown/showdown.js
+++ showdown/showdown.js
@@ -16,7 +16,62 @@ document.addEventListener('DOMContentLoaded', function() {
      });
 
      loadMD('pediatria/pediatria.md');
-   
+     document.getElementById('inicio').innerHTML = '';
+  
+  document.getElementById('tabla_farmacos').innerHTML = '<input type="text" id="search" placeholder="Type to search"></input>';
+  document.getElementById('tabla_farmacos').innerHTML += '<table class="table" id="la-tabla"><thead><tr id="nombres_cols"> </tr></thead><tbody id="cuerpo_tabla"></tbody></table>';
+  document.getElementById('tabla_farmacos').setAttribute('class',"tab-pane container active");
+  let SHEET_ID = '1SMU1ltLrMVifOb2T8sN5gu5Yd3tKmQ6eid6QnjSDlQo';
+  let SHEET_TITLE = 'dosis';
+  let SHEET_RANGE = 'A1:G248'
+  let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
+  console.log('hola');
+  fetch(FULL_URL)
+  .then(res => res.text())
+  .then(rep => {
+      let data = JSON.parse(rep.substr(47).slice(0,-2));
+      let tablitaLinda = data.table.rows; 
+      console.log(tablitaLinda);
+      for (let i = 0; i < tablitaLinda[0].c.length; i++) {
+          const element = tablitaLinda[0].c[i].v;
+          console.log(element);
+          console.log('hola mundo desde el for');
+          document.getElementById('nombres_cols').innerHTML += `<th id="${element}">${element}</th>` 
+
+
+      }
+      for (let i = 1; i < tablitaLinda.length; i++) {
+          const element = tablitaLinda[i].c;
+          const fila = `<tr id="fila_${i}"></tr>`;
+          document.getElementById('cuerpo_tabla').innerHTML += fila;
+          for (let j = 0; j < element.length; j++) {
+              if (element[j] == null) {
+                  document.getElementById(`fila_${i}`).innerHTML += '<td>NA</td>';
+              }else{
+                  if (element[j].v == null) {
+                      document.getElementById(`fila_${i}`).innerHTML += '<td>NA</td>';
+                  } else{
+                      const dato = element[j].v;
+                      const celda = `<td>${dato}</td>`;
+                      document.getElementById(`fila_${i}`).innerHTML += celda;
+                  }
+              }
+          }
+      }
+      
+  })
+  .then(rep => {
+    var $rows = $('#cuerpo_tabla tr');
+    $('#search').keyup(function() {
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+  
+        $rows.show().filter(function() {
+            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+            return !~text.indexOf(val);
+        }).hide();
+    });
+
+  })
 });
 
 function loadMD(filepath) {
@@ -45,6 +100,8 @@ function loadMD(filepath) {
         image.setAttribute('src', fileFolder + currentSrc);
       }     
       document.getElementsByTagName('main')[0].style.maxWidth = '800px'
+      document.getElementById('inicio').setAttribute('class',"tab-pane container active");
+      document.getElementById('tabla_farmacos').setAttribute('class',"tab-pane container");
     })
     .catch(err => {
       console.log(err);
